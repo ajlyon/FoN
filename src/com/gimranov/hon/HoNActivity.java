f38e6fcede8fb0cfa74fbe32090119ca38352aa5
Avram Lyon <ajlyon@gmail.com>
2012-01-15 15:19:38 -0800

    Initial template commit

---
+++ src/com/gimranov/hon/HoNActivity.java
@@ -0,0 +1,13 @@
+package com.gimranov.hon;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class HoNActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
